f8a5d4ee79a46350a4e9a3202fa10c526989b6cc
eyuphan-oguz <eyuphanoguz@yahoo.com>
2023-04-10 01:43:29 +0300

    create page watchLater

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:movieproject/product/widgets/watch_later_content_widget.dart';
 import 'package:movieproject/screens/content_page_view.dart';
 import 'package:movieproject/screens/home_page_view.dart';
 import 'package:movieproject/screens/introduction_page_view.dart';
@@ -37,7 +38,7 @@ class MyApp extends StatelessWidget {
         
       ),
       debugShowCheckedModeBanner: false,
-      home: RouterPageView(),
+      home: MoviesPage(),
     );
   }
 }
