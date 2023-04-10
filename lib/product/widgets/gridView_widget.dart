30fd39a54ab0f375dbbd5d0a8c3178b1da5477ff
eyuphan-oguz <eyuphanoguz@yahoo.com>
2023-04-10 23:56:15 +0300

    create gridview widget

---
+++ lib/product/widgets/gridView_widget.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class GridViewWidget extends StatefulWidget {
+  const GridViewWidget({super.key});
+
+  @override
+  State<GridViewWidget> createState() => _GridViewWidgetState();
+}
+
+class _GridViewWidgetState extends State<GridViewWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
